ddd16d7bbde57cac6d762cb035f9bb7de07b6966
lars50p <reg50p@comhem.se>
2025-10-17 19:18:15 +0200

    small fix

--- src/main/kotlin/femtioprocent/ansi/appl/AnsiDemo.kt
+++ src/main/kotlin/femtioprocent/ansi/appl/AnsiDemo.kt
@@ -377,8 +377,9 @@ class AnsiDemo {
                 val verb = true
                 val N = 24
                 true.let {
-                    val rgbRand0 = RGB(cs=256, r=203, g=107, b=108) // randomRGB(256).average(RGB(3, 2, 1, 0))
-                    println("Base color: ${rgbRand0.showL()}  ${rgbRand0.toHsv()}")
+		    val other = randomRGB(256).toSaturation(Random.nextDouble(1.0))
+                    val rgbRand0 = randomRGB(256).average(other)
+                    println("Base color: ${rgbRand0.showL()}  ${rgbRand0.toHsv()}  -- other: ${other.showL()}  ${other.toHsv()}")
                     repeat(N) { n ->
                         val value = n.toDouble() / 54
                         val hsv = rgbRand0.toHsv()
@@ -388,7 +389,7 @@ class AnsiDemo {
                         //rgbRand.forEach { println("  " + it.showR()) }
                         (1..2*N).forEach {
                            rgb2 = rgb2.average(rgb2.toValue(0.2), 0.1 + 0.3 / it.toDouble())
-                            print(" ${formatter3(rgb2, ' ')} ".rgbBg(rgb2))
+                            print("${formatter3(rgb2, ' ')} ".rgbBg(rgb2))
                         }
                         println()
                     }
