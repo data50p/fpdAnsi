a8eb22ef330d173efe81821681a198eed4763ab0
lars50p <reg50p@comhem.se>
2025-10-23 12:43:58 +0200

    cleanup

--- src/main/kotlin/femtioprocent/ansi/Version.kt
+++ src/main/kotlin/femtioprocent/ansi/Version.kt
@@ -1,14 +1,13 @@
 package femtioprocent.ansi
 
 object Version {
-    fun info(): String {
-        return "$projectVersion $buildHost $buildTimestamp ($buildNumber)"
+    fun info() : String {
+	return "$projectVersion $buildHost $buildTimestamp ($buildNumber)"
     }
-
     const val projectGroup: String = "femtioprocent"
     const val projectName: String = "ansi"
     const val projectVersion: String = "0.0.1.0"
-    const val buildTimestamp: String = "2025-10-13 11:31:56 +0200"
-    const val buildHost = "mango-28.local"
-    const val buildNumber = "283"
+    const val buildTimestamp: String = "2025-10-23 12:43:20 +0200"
+    const val buildHost = "apelsin-3.local"
+    const val buildNumber = "1153"
 }
